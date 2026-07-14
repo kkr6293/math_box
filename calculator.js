d6cf5b4dcdbf9444a145c595631ce033dfd3fe59
KKR <kr@KKRui-MacBookAir.local>
2026-07-14 11:24:04 +0900

    add comm

--- calculator.js
+++ calculator.js
@@ -1,12 +1,22 @@
-function add(a, b) {
-  return a + b;
-}
+// 덧셈
+const add = (a, b) => {
+    return a + b;
+};
 
-//
-function substract(a, b) {
-  return a - b;
-}
+// 뺄셈
+const substract = (a, b) => {
+    return a - b;
+};
 
+// 곱셈
 const multiply = (a, b) => {
-  return a * b;
+    return a * b;
+};
+
+// 나눗셈
+const divide = (a, b) => {
+    if (b === 0) {
+        throw new Error();
+    }
+    return a / b;
 };
